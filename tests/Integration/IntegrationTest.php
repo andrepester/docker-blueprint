bdf1f4df77fdb985ef4340222b018d6592cb9fe8
André Pester <mail@andre-pester.de>
2018-07-19 13:26:28 +0200

    adding integrationtest

---
+++ tests/Integration/IntegrationTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace DockerBlueprint\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class IntegrationTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
