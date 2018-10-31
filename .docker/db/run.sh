d1c261099363c64d3bb26e4d4bed46f592586002
André Pester <mail@andre-pester.de>
2018-10-31 23:21:30 +0100

    fixing user config

--- .docker/db/run.sh
+++ .docker/db/run.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-if [ ! -d '/run/mysqld' ]; then
+if [ ! -d "/run/mysqld" ]; then
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
 fi
 
 if [ -d /var/lib/mysql/mysql ]; then
-	echo '[Docker DB] data directory already created'
+	echo "[Docker DB] data directory already created"
 else
-	echo '[Docker DB] create data directory'
+	echo "[Docker DB] create data directory"
 
 	chown -R mysql:mysql /var/lib/mysql
 
-	echo '[Docker DB] init DB'
+	echo "[Docker DB] init DB"
 
 	mysql_install_db --user=mysql > /dev/null
 
@@ -25,15 +25,15 @@ else
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
 EOF
 
-	echo '[Docker DB] configure user'
+	echo "[Docker DB] execute mysql with: $tfile"
 
 	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
 
 	rm -f $tfile
 fi
 
-echo '[Docker DB] start process'
+echo "[Docker DB] start process"
 exec /usr/bin/mysqld --user=mysql --console
